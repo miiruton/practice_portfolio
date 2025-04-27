5f3549e581ea43fbba6091619a65839d8642b859
Milton <miltonrafaelcastro@gmail.com>
2025-04-27 10:15:52 -0300

    Improve css and fix js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function clickReveal() {
+function clickRevealone() {
     var x = document.getElementById("projectsone");
     if (x.style.display === "none") {
       x.style.display = "block";
@@ -8,10 +8,44 @@ function clickReveal() {
   } 
 
 
+  function clickRevealtwo() {
+    var x = document.getElementById("projectstwo");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
+
+
+
   function changeImage() {
-    if (document.getElementById("eyeIcon").src == "icons/eye-closed3.svg"){
+    if (document.getElementById("eyeIcon").src == "icons\eye-closed3.svg"){
         document.getElementById("eyeIcon").src = "icons\show.svg";
     } else {
         document.getElementById("eyeIcon").src = "icons\eye-closed3.svg";
     }
 }
+
+function changeImage2() {
+  if (document.getElementById("eyeIcon2").src == "icons\eye-closed3.svg"){
+      document.getElementById("eyeIcon2").src = "icons\show.svg";
+  } else {
+      document.getElementById("eyeIcon2").src = "icons\eye-closed3.svg";
+  }
+}
+
+
+/*const myImg = document.getElementById("eyeIcon");
+const myButton = document.getElementById("projectdetail");
+
+let isFirstImage = true;
+
+myButton.addEventListener("click", function() {
+  if (isFirstImage) {
+    myImg.src = "icons\eye-closed3.svg";
+  } else {
+    myImg.src = "icons\show.svg";
+  }
+  isFirstImage = !isFirstImage;
+});*/
