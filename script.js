87808364024efd060f80cc239f9a60d1f2b15bce
Milton <miltonrafaelcastro@gmail.com>
2025-04-27 12:21:52 -0300

    js and css experimentation

--- script.js
+++ script.js
@@ -9,34 +9,66 @@ function clickReveal1() {
     }
   }
 
-  document.getElementById("projectstwo").style.display = 'none';
+document.getElementById("projectstwo").style.display = 'none';
 
-  function clickReveal2() {
-    var x = document.getElementById("projectstwo");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  } 
+function clickReveal2() {
+  var x = document.getElementById("projectstwo");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
 
 
+document.getElementById("projectsthree").style.display = 'none';
 
-  function changeImage1() {
-    if (document.getElementById("eyeIcon").src == "icons\eye-closed3.svg"){
-        document.getElementById("eyeIcon").src = "icons\show.svg";
-    } else {
-        document.getElementById("eyeIcon").src = "icons\eye-closed3.svg";
+function clickReveal3() {
+  var x = document.getElementById("projectsthree");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+
+
+function changeImage1() {
+  if (document.getElementById("eyeIcon").src == "icons\eye-closed3.svg"){
+     document.getElementById("eyeIcon").src = "icons\show.svg";
+  } else {
+     document.getElementById("eyeIcon").src = "icons\eye-closed3.svg";
     }
 }
 
+
 function changeImage2() {
+  var icon = document.getElementById("eyeIcon2");
+  icon.classList.toggle('show-icon');
+  icon.classList.toggle('hide-icon');
+}
+
+/*function changeImage2() {
   if (document.getElementById("eyeIcon2").src == "icons\eye-closed3.svg"){
       document.getElementById("eyeIcon2").src = "icons\show.svg";
   } else {
       document.getElementById("eyeIcon2").src = "icons\eye-closed3.svg";
   }
+}*/
+
+function changeImage3() {
+  var icon = document.getElementById("eyeIcon3");
+  icon.classList.toggle('fa-eye-slash');
+  icon.classList.toggle('fa-eye');
 }
+
+/*function changeImage2() {
+  if (document.getElementById("eyeIcon2").class == "fa-eye-slash"){
+      document.getElementById("eyeIcon2").class = "fa-eye";
+  } else {
+      document.getElementById("eyeIcon2").class = "fa-eye-slash";
+  }
+}*/
 
 
 /*const myImg = document.getElementById("eyeIcon");
